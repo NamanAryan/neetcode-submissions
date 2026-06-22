59d1a343d7d914e2f0c4d3b54fc75118d18280aa
Naman Aryan <160505477+NamanAryan@users.noreply.github.com>
2026-06-23 01:20:28 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+        q = deque()
+        direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        for r in range(ROWS):
+            for c in range(COLS):
+                
+                if grid[r][c] == 0:
+                    q.append((r, c))
+                    visit.add((r, c))
+        dist = 0
+        while q:
+            for _ in range(len(q)):
+                R, C = q.popleft()
+                grid[R][C] = dist
+                
+                for dr, dc in direction:
+                    row = dr + R
+                    col = dc + C
+                    if (row < 0 or
+                        col < 0 or
+                        row >= ROWS or
+                        col >= COLS or
+                        grid[row][col] == -1 or
+                        (row, col) in visit):
+                        continue
+                    visit.add((row, col))
+                    q.append([row, col])
+            dist += 1
+
+
